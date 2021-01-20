141ec9291482731eb8e13a3a64b932cc8a9f3b28
Neil Foster <neilfoster81@gmail.com>
2021-01-20 16:50:16 +0000

    Added const inside of function

--- src/index.js
+++ src/index.js
@@ -6,27 +6,19 @@ function Booklist() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
 const Book = () => {
+  const title = 'The Very Hungry Caterpillar'
   return <article className='book'>
-    <Image></Image>
-    <Title></Title>
-    <Author></Author>
+    <img src="https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg" alt="" />
+    <h1>{title}</h1>
+    <h4>Eric Carle</h4>
   </article>;
 };
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg" alt="" />
-
-const Title = () => <h1>The very Hungry Caterpillar</h1>
-
-const Author = () => <h4 style={{ color: '617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>Eric Carle</h4>
 
 ReactDom.render(<Booklist />, document.getElementById('root')
 );
