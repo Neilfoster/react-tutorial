457ae2d0127388fd7617fb23f5a07c29063f3f6d
Neil Foster <neilfoster81@gmail.com>
2021-01-21 11:26:18 +0000

    Added props

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+// CSS
 import './style.css';
+
+//setup vars
+const title = 'The Very Hungry Caterpillar';
+const author = 'Eric Carle';
+const img = "https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg";
 
 function Booklist() {
   return (
     <section className='booklist'>
+      <Book job="developer" />
+      <Book title='random title' number={22} />
+      <Book />
       <Book />
     </section>
   );
 }
 
-const Book = () => {
-  const title = 'The Very Hungry Caterpillar'
+
+const Book = (props) => {
+  console.log(props)
   return <article className='book'>
-    <img src="https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg" alt="" />
+    <img src={img} alt='' />
     <h1>{title}</h1>
-    <h4>Eric Carle</h4>
+    <h4>{author}</h4>
+    <p>{props.job}</p>
+    <p>{props.title}</p>
+    <p>{props.number}</p>
   </article>;
 };
 
