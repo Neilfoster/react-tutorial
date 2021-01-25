441b1951d52c0aef0a2a20e6b1b1c4f167a89a10
Neil Foster <neilfoster81@gmail.com>
2021-01-25 12:26:02 +0000

    Added 2 different types of object destructuring for props

--- src/index.js
+++ src/index.js
@@ -26,13 +26,15 @@ function Booklist() {
 }
 
 
-const Book = (props) => {
-  console.log(props)
-  return <article className='book'>
-    <img src={props.img} alt='' />
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>
-  </article>;
+const Book = ({ img, title, author }) => {
+  //const { img, title, author } = props
+  return (
+    <article className='book'>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
 };
 
 
